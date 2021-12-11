fe080cbdf2006a62183a6f69afdc718099ea5ac6
adarbayan <66789277+adarbayan@users.noreply.github.com>
2021-12-11 05:04:50 +0300

    Add files via upload

---
+++ Quiz_App/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,24 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 07/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    
+    let answers: [String]
+    let rightAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        rightAnswer = correctAnswer
+    }
+}
+
+    
